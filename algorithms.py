7c82f6bb2ec481e98371a9d88e8c203c694ac5d9
antal36 <antal.tom9@gmail.com>
2024-06-07 15:48:22 +0200

    annotations

--- algorithms.py
+++ algorithms.py
@@ -8,46 +8,51 @@ from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.base import BaseEstimator
+from typing import Iterator
 
-# Load datasets
-df1 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Cardiovascular_Disease_Dataset_final.csv")
-df2 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/dataset_37_diabetes_final.csv")
-df3 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/diabetes_classification_final.csv")
-df4 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Medicaldataset_final.csv")
+"""Load datasets"""
+df1: pd.DataFrame = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Cardiovascular_Disease_Dataset_final.csv")
+df2: pd.DataFrame = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/dataset_37_diabetes_final.csv")
+df3: pd.DataFrame = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/diabetes_classification_final.csv")
+df4: pd.DataFrame = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Medicaldataset_final.csv")
 
-# Define datasets
-datasets = {
+datasets: dict[str, BaseEstimator] = {
     "Cardiovascular_Disease_Dataset": df1,
     "Diabetes_Dataset_37": df2,
     "Diabetes_Classification": df3,
     "Medical_Dataset": df4
 }
 
-algorithms = [LogisticRegression, RandomForestClassifier, GaussianNB, DecisionTreeClassifier, KNeighborsClassifier]
+algorithms: list[BaseEstimator] = [LogisticRegression, RandomForestClassifier, GaussianNB, DecisionTreeClassifier, KNeighborsClassifier]
 
-result = {}
+result: dict[str, dict[str, dict[str, float]]] = {}
 
-def get_combination(lst):
+def get_combination(columns: list[str]) -> Iterator[tuple[str]]:
     combination = []
-    for r in range(1, len(lst) + 1):
-        combination.extend(itertools.combinations(lst,r))
+    for r in range(1, len(columns) + 1):
+        combination.extend(itertools.combinations(columns, r))
+
     yield from combination
 
-def evaluate_algorithm(algorithm):
+def evaluate_algorithm(algorithm: BaseEstimator) -> None:
+    """Evaluate the specified machine learning algorithm on each dataset using all possible combinations of features."""
+
     algorithm_name = algorithm.__name__
     result[algorithm_name] = {}
     
+    dataset_name: str
+    df: pd.DataFrame
     for dataset_name, df in datasets.items():
 
-        columns = list(df.columns)
+        columns: list[str] = list(df.columns)
 
         result[algorithm_name][dataset_name] = {}
 
         for combination in get_combination(columns[:-1]):
             combination = sorted(combination)
-            print(algorithm_name, combination)
-            X = df.loc[:,combination]    
-            y = df.iloc[:, -1]
+            X: pd.DataFrame = df.loc[:, combination]    
+            y: pd.DataFrame = df.iloc[:, -1]
 
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             
@@ -59,9 +64,12 @@ def evaluate_algorithm(algorithm):
             
             result[algorithm_name][dataset_name][", ".join(combination)] = accuracy
 
+algorithm: BaseEstimator
 for algorithm in algorithms:
     evaluate_algorithm(algorithm)
+"""Evaluate each algorithm"""
 
 json_object = js.dumps(result, indent=4)
 with open("results.json", "w") as outfile:
     outfile.write(json_object)
+"""Save the result dictionary into JSON file"""
