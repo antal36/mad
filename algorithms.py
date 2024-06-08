35f7e05549115d71af9e475a17490ad9ca22ed1a
antal36 <antal.tom9@gmail.com>
2024-06-08 10:19:41 +0200

    new graphs

--- algorithms.py
+++ algorithms.py
@@ -4,50 +4,53 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.base import BaseEstimator
+from typing import Iterator, List, Dict, Tuple
 
-# Load datasets
-df1 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Cardiovascular_Disease_Dataset_final.csv")
-df2 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/dataset_37_diabetes_final.csv")
-df3 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/diabetes_classification_final.csv")
-df4 = pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Medicaldataset_final.csv")
+"""Loading datasets"""
+df1: pd.DataFrame = pd.read_csv("data/Cardiovascular_Disease_Dataset_final.csv")
+df2: pd.DataFrame = pd.read_csv("data/dataset_37_diabetes_final.csv")
+df3: pd.DataFrame = pd.read_csv("data/diabetes_classification_final.csv")
+df4: pd.DataFrame = pd.read_csv("data/Medicaldataset_final.csv")
 
-# Define datasets
-datasets = {
+datasets: Dict[str, pd.DataFrame] = {
     "Cardiovascular_Disease_Dataset": df1,
-    "Diabetes_Dataset_37": df2,
+    "Diabetes_Dataset_37": df2, 
     "Diabetes_Classification": df3,
     "Medical_Dataset": df4
 }
 
-algorithms = [LogisticRegression, RandomForestClassifier, GaussianNB, DecisionTreeClassifier, KNeighborsClassifier]
+algorithms: List[BaseEstimator] = [LogisticRegression, RandomForestClassifier, GaussianNB, DecisionTreeClassifier, KNeighborsClassifier]
+"""Algorithms to be compared"""
+result: Dict[str, Dict[str, Dict[str, float]]] = {}
 
-result = {}
-
-def get_combination(lst):
-    combination = []
-    for r in range(1, len(lst) + 1):
-        combination.extend(itertools.combinations(lst,r))
+def get_combination(columns: List[str]) -> Iterator[Tuple[str]]:
+    """Yields all possible combinations of columns"""
+    combination: List[Tuple[str]] = []
+    for r in range(1, len(columns) + 1):
+        combination.extend(itertools.combinations(columns, r))
     yield from combination
 
-def evaluate_algorithm(algorithm):
+def evaluate_algorithm(algorithm) -> None:
+    """Evaluate the specified machine learning algorithm on each dataset using all possible combinations of features.
+       Evaluation metrics: Accuracy score, Precision score, Recall score, F1 score, ROC AUC"""
     algorithm_name = algorithm.__name__
     result[algorithm_name] = {}
     
+    dataset_name: str
+    df: pd.DataFrame
     for dataset_name, df in datasets.items():
-
         columns = list(df.columns)
-
         result[algorithm_name][dataset_name] = {}
 
         for combination in get_combination(columns[:-1]):
             combination = sorted(combination)
-            print(algorithm_name, combination)
-            X = df.loc[:,combination]    
-            y = df.iloc[:, -1]
+            X: pd.DataFrame = df.loc[:, combination]    
+            y: pd.DataFrame = df.iloc[:, -1]
 
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             
@@ -55,9 +58,21 @@ def evaluate_algorithm(algorithm):
             model.fit(X_train, y_train)
 
             y_pred = model.predict(X_test)
-            accuracy = accuracy_score(y_test, y_pred)
+            y_prob = model.predict_proba(X_test)[:, 1] if hasattr(model, "predict_proba") else None
             
-            result[algorithm_name][dataset_name][", ".join(combination)] = accuracy
+            accuracy: float = accuracy_score(y_test, y_pred)
+            precision: float = precision_score(y_test, y_pred)
+            recall: float = recall_score(y_test, y_pred)
+            f1: float = f1_score(y_test, y_pred)
+            roc_auc: float = roc_auc_score(y_test, y_prob) if y_prob is not None else None
+
+            result[algorithm_name][dataset_name][", ".join(combination)] = {
+                "accuracy": accuracy,
+                "precision": precision,
+                "recall": recall,
+                "f1_score": f1,
+                "roc_auc": roc_auc
+            }
 
 for algorithm in algorithms:
     evaluate_algorithm(algorithm)
@@ -65,3 +80,7 @@ for algorithm in algorithms:
 json_object = js.dumps(result, indent=4)
 with open("results.json", "w") as outfile:
     outfile.write(json_object)
+"""The result dictionary is written into JSON file"""
+
+
+print("Done")
--- project/app.py
+++ project/app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import json
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from typing import Any, Union, List, Dict
+from sklearn.base import BaseEstimator
 
 app = Flask(__name__)
 
-datasets = {
+datasets: Dict[str, Dict[str, Any]] = {
     "Cardiovascular diseases (India)": {
-        "data": pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Cardiovascular_Disease_Dataset_final.csv"),
+        "data": pd.read_csv("data/Cardiovascular_Disease_Dataset_final.csv"),
         "description": """The source of the data: https://data.mendeley.com/datasets/dzz48mvjht/1 \n
         This heart disease dataset is sourced from a multispecialty hospital in India.
         It includes data from 1,000 subjects, encompassing 12 key features relevant to heart disease.
@@ -21,32 +21,32 @@ datasets = {
         With its comprehensive feature set, this dataset serves as a valuable resource for advancing heart disease research and improving diagnostic methodologies."""
     },
     "Cardiovascular diseases (Iraq)": {
-        "data": pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/Medicaldataset_final.csv"),
+        "data": pd.read_csv("data/Medicaldataset_final.csv"),
         "description": """The source of the data: https://data.mendeley.com/datasets/wmhctcrt5v/1 \n
         The heart attack dataset was collected at Zheen Hospital in Erbil, Iraq, from January 2019 to May 2019.
         The attributes of this dataset include age, gender, heart rate, systolic blood pressure, diastolic blood pressure, blood sugar, CK-MB, and troponin levels, along with a classification output indicating either a heart attack or no heart attack.
         The gender column is normalized, with males set to 1 and females to 0. The blood sugar (glucose) column is set to 1 if the level is greater than 120, otherwise, it is set to 0. The classification output is set to 1 for positive (heart attack) and 0 for negative (no heart attack)."""
     },
     "Diabetes (America)": {
-        "data": pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/dataset_37_diabetes_final.csv"),
+        "data": pd.read_csv("data/dataset_37_diabetes_final.csv"),
         "description": """The source of the data http://biostat.mc.vanderbilt.edu/wiki/Main/DataSets.\n 
         This dataset includes several hundred rural African-American patients. Patients without a hemoglobin A1c measurement were excluded.
         Those with a hemoglobin A1c of 6.5 or greater were labeled as diabetic (diabetes = yes). Out of 390 patients, 60 were found to be diabetic."""
     },
     "Diabetes (Pima Indians)": {
-        "data": pd.read_csv("C:/Users/antal/Desktop/matfyz/MAD/mad/project/data/diabetes_classification_final.csv"),
+        "data": pd.read_csv("data/diabetes_classification_final.csv"),
         "description": """The source of the data: https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database \n
         This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases.
         The objective of the dataset is to diagnostically predict whether a patient has diabetes based on certain diagnostic measurements.
         All patients are females at least 21 years old of Pima Indian heritage. The dataset consists of several medical predictor variables and one target variable, Outcome.
         Predictor variables include the number of pregnancies, BMI, insulin level, age, and other relevant medical measurements."""
-} 
-    } 
+    }
+}
 
-with open("C:/Users/antal/Desktop/matfyz/MAD/mad/results.json", 'r') as file:
+with open("../results.json", 'r') as file:
     performance_metric = json.load(file)
 
-algorithms = {
+algorithms: Dict[str, BaseEstimator] = {
     "Logistic Regression": LogisticRegression,
     "Random Forest": RandomForestClassifier,
     "Naive Bayes": GaussianNB,
@@ -56,10 +56,12 @@ algorithms = {
 
 @app.route('/')
 def index():
+    """Render the index page with dataset descriptions."""
     return render_template('index.html', datasets={name: data["description"] for name, data in datasets.items()})
 
 @app.route('/dataset/<dataset_name>', methods=['GET', 'POST'])
 def dataset(dataset_name):
+    """Render the dataset page and handle column selection and algorithm choice."""
     dataset = datasets[dataset_name]["data"]
     description = datasets[dataset_name]["description"]
     if request.method == 'POST':
@@ -69,10 +71,11 @@ def dataset(dataset_name):
     return render_template('dataset.html', dataset_name=dataset_name, columns=dataset.columns, algorithms=algorithms, description=description)
 
 @app.route('/algorithm/<dataset_name>/<algorithm_name>/<columns>', methods=['GET', 'POST'])
-def algorithm(dataset_name, algorithm_name, columns):
-    dataset = datasets[dataset_name]
-    selected_columns = columns.split(',')
-    performance = get_model_performance(dataset_name, selected_columns, algorithm_name)
+def algorithm(dataset_name: str, algorithm_name: str, columns: str):
+    """Render the algorithm performance page."""
+
+    selected_columns: List[str] = columns.split(',')
+    performance: Union[Dict[str, float], str] = get_model_performance(dataset_name, selected_columns, algorithm_name)
     if request.method == 'POST':
         second_algorithm_name = request.form['second_algorithm']
         second_performance = get_model_performance(dataset_name, selected_columns, second_algorithm_name)
@@ -85,16 +88,18 @@ def algorithm(dataset_name, algorithm_name, columns):
         return render_template('compare.html', comparison_data=comparison_data, algorithms=algorithms)
     return render_template('algorithm.html', performance=performance, algorithm_name=algorithm_name, columns=columns, algorithms=algorithms.keys())
 
+def get_model_performance(dataset, selected_columns, algorithm_name) -> Union[Dict[str, float], str]:
+    """Get the performance of the selected model on the selected dataset and columns."""
 
-def get_model_performance(dataset, selected_columns, algorithm_name):
-    print(selected_columns)
-    columns_key = ', '.join(sorted(selected_columns))
-    name_algo = get_right_name_algo(algorithm_name)
-    name_data = get_right_name_data(dataset)
-    performance = performance_metric[name_algo][name_data].get(columns_key, "No data available")
+    columns_key: str = ', '.join(sorted(selected_columns))
+    name_algo: str = get_right_name_algo(algorithm_name)
+    name_data: str = get_right_name_data(dataset)
+    performance: Union[Dict[str, float], str] = performance_metric[name_algo][name_data].get(columns_key, "No data available")
     return performance
 
-def get_right_name_algo(name):
+def get_right_name_algo(name: str) -> str:
+    """Convert user-friendly algorithm name to the corresponding class name."""
+
     match name:
         case "Logistic Regression":
             return "LogisticRegression"
@@ -107,7 +112,9 @@ def get_right_name_algo(name):
         case "K nearest neighbours":
             return "KNeighborsClassifier"
 
-def get_right_name_data(name):
+def get_right_name_data(name: str) -> str:
+    """Convert user-friendly dataset name to the corresponding key in performance metrics."""
+
     match name:
         case "Cardiovascular diseases (India)":
             return "Cardiovascular_Disease_Dataset"
@@ -117,7 +124,6 @@ def get_right_name_data(name):
             return "Diabetes_Dataset_37"
         case "Diabetes (Pima Indians)":
             return "Diabetes_Classification"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
